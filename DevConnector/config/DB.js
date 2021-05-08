bf495ab8464982e207bee165613d774ec22d67f6
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-05-09 02:44:39 +0530

    define basic test router

--- DevConnector/config/DB.js
+++ DevConnector/config/DB.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async ()=>{
   try{
-      await mongoose.connect(db,{useNewUrlParser:true});
+      await mongoose.connect(db,{useNewUrlParser:true, useUnifiedTopology:true});
       console.log('MongoDB connected!');
   }catch(err){
     console.log(err.message);
