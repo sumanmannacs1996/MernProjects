5d29ada93401a71f11246583777e9d537bd6332e
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-05-12 00:05:45 +0530

    Added Post Schema

---
+++ DevConnector/models/Post.js
@@ -0,0 +1,61 @@
+const mongoose =require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    user:{
+        type:Schema.type.ObjectId,
+        ref:'user'
+    },
+    text:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    name:{
+        type:String,
+        trim:true
+    },
+    avatar:{
+        type:String
+    },
+    likes:[
+        {
+            users:{
+                type:Schema.type.ObjectId,
+                ref:'user'
+            }
+        }
+    ],
+    comments:[
+        {
+            users:{
+                type:Schema.type.ObjectId,
+                ref:'user'
+            },
+            text:{
+                type:String,
+                trim:true,
+                required:true
+            },
+            name:{
+                type:String,
+                trim:true
+            },
+            avatar:{
+                type:String
+            },
+            date:{
+                type:Date,
+                default:Date.now
+            }
+
+        }
+    ],
+    date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+module.exports = Post = mongoose.model('post',PostSchema);
