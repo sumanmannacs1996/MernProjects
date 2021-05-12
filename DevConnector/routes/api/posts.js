ab4ed36a94fd3c646dda9e583f3eb8bcf1c2ce41
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-05-13 00:09:19 +0530

    Added route for add comment to a post using post id

--- DevConnector/routes/api/posts.js
+++ DevConnector/routes/api/posts.js
@@ -139,5 +139,33 @@ router.put('/unlike/:post_id',auth, async (req,res)=>{
         console.log(err.message);
         res.status(500).send("Server Error");
     }
-})
+});
+
+// @route   POST api/posts/comment/:id
+// @desc    Comment on a post
+// @access  Private
+router.post('/comment/:post_id',[auth,[
+    check('text',"Text is requird").not().isEmpty()
+]], async(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.post_id);
+        const newComment = {
+            user:req.user.id,
+            text:req.body.text,
+            name:user.name,
+            avatar:user.avatar
+        };
+        post.comments.unshift(newComment);
+        await post.save();
+        res.json(post.comments);
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send("Server Error");
+    }
+});
 module.exports= router;
