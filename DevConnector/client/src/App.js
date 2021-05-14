0c58190672d7a121067d645b51013e01b20b5622
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-05-14 19:02:39 +0530

    Added route for login and register and code added to go to component register and login using Link

--- DevConnector/client/src/App.js
+++ DevConnector/client/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 function App() {
   return (
@@ -10,6 +12,12 @@ function App() {
       <Fragment>
         <Navbar/>
         <Route exact path="/" component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/login' component={Login}/>
+            <Route exact path='/register' component={Register}/>
+          </Switch>
+        </section>
       </Fragment>
     </BrowserRouter>
   );
