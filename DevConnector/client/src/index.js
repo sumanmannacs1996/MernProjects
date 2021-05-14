6f06ee26ade2c5e34e5e841122f3c38a821f35f3
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-05-14 20:42:24 +0530

    Aded Redux store and added config for store and added combined dummy reducer

--- DevConnector/client/src/index.js
+++ DevConnector/client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {Provider} from 'react-redux'
+import store from './components/store/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store ={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
