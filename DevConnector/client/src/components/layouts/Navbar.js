be027e52059b7cd61817ef7bd6257ecb3501ee14
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-05-15 20:08:36 +0530

    Added action for logout, Added reducer for logout, Connect component to action and dynamic change nave bar links

--- DevConnector/client/src/components/layouts/Navbar.js
+++ DevConnector/client/src/components/layouts/Navbar.js
@@ -1,18 +1,49 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-const Navbar = () => {
+import React, { Fragment } from 'react'
+import {Link, Redirect} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {logout} from '../../actions/auth';
+import PropTypes from 'prop-types'
+
+const Navbar = (props) => {
+    console.log(props.auth.isAuthenticated);
+
+    const authLinks =(
+        <ul>
+            <li>
+                <a onClick={()=>props.logout()} href='/'>
+                    <i className='fas fa-sign-out-alt'/>{' '}
+                    <span className='hide-sm-'>Logout</span>
+                </a>
+            </li>
+        </ul>
+    );
+
+    const guestLinks=(
+        <ul>
+            <li><Link to="/">Developers</Link></li>
+            <li><Link to="/register">Register</Link></li>
+            <li><Link to="/login">Login</Link></li>
+        </ul>
+    );
     return (
         <nav className="navbar bg-dark">
             <h1>
                 <Link to="index.html"><i className="fas fa-code"></i> DevConnector</Link>
             </h1>
-            <ul>
-                <li><Link to="/">Developers</Link></li>
-                <li><Link to="/register">Register</Link></li>
-                <li><Link to="/login">Login</Link></li>
-            </ul>
+            {!props.auth.loading && 
+            <Fragment>
+                {props.auth.isAuthenticated ? authLinks : guestLinks}
+            </Fragment>}
         </nav>
     )
 }
+Navbar.propsType={
+    logout:PropTypes.func.isRequired,
+    auth:PropTypes.object.isRequired,
+}
 
-export default Navbar
+const mapStateToProps=state=>({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps,{logout})(Navbar);
--- DevConnector/client/src/reducers/auth.js
+++ DevConnector/client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import {REGISTER_SUCCESS,REGISTER_FAIL,USER_LOADED,AUTH_ERROR,LOGIN_SUCCESS,LOGIN_FAIL} from '../actions/types';
+import {REGISTER_SUCCESS,REGISTER_FAIL,USER_LOADED,AUTH_ERROR,LOGIN_SUCCESS,LOGIN_FAIL,LOGOUT} from '../actions/types';
 const initialState = {
     token:localStorage.getItem('token'),
     isAuthenticated:false,
@@ -21,7 +21,8 @@ export default function(state =initialState,action){
 
         case REGISTER_FAIL:
         case AUTH_ERROR:
-        case LOGIN_FAIL:       
+        case LOGIN_FAIL:
+        case LOGOUT :           
             //removing token from Local storage
             localStorage.removeItem('token');
             return{
