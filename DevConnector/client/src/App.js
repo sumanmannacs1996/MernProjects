e0b8450efad553cda1988bfd7826dbc47f57173c
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-05-16 16:00:40 +0530

    Added component for AddEducation and connect it to addEducation action

--- DevConnector/client/src/App.js
+++ DevConnector/client/src/App.js
@@ -14,6 +14,7 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 
 
 function App(props) {
@@ -34,6 +35,7 @@ function App(props) {
             <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
             <PrivateRoute exact path='/add-experience' component={AddExperience} />
+            <PrivateRoute exact path='/add-education' component={AddEducation} />
           </Switch>
         </section>
       </Fragment>
