57bad466faa19091f464ea8f50de46d2ac03fffb
sumanmannacs1996 <sumanmannacs1996@gmail.com>
2021-05-18 23:53:42 +0530

    Added component Profiles and ProfileItem and connect getAllProfiles action to show data in the screen

--- DevConnector/client/src/components/layouts/Navbar.js
+++ DevConnector/client/src/components/layouts/Navbar.js
@@ -8,6 +8,9 @@ const Navbar = (props) => {
     const authLinks =(
         <ul>
             <li>
+                <Link to="/profiles">Developers</Link>
+            </li>
+            <li>
                 <Link to="/dashboard">
                 <i className='fas fa-user'/> {' '}
                 <span className='hide-sm'>Dashboard</span>
@@ -24,7 +27,7 @@ const Navbar = (props) => {
 
     const guestLinks=(
         <ul>
-            <li><Link to="/">Developers</Link></li>
+            <li><Link to="/profiles">Developers</Link></li>
             <li><Link to="/register">Register</Link></li>
             <li><Link to="/login">Login</Link></li>
         </ul>
